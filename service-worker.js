5270d2bd2298c26b414e0c54390dd615d4cd913f
MadushanR <madushanrajendran@gmail.com>
2025-11-16 17:41:21 -0500

    Added Search and Expiry Date Capture

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "expiry-tracker-v1";
+const CACHE_NAME = "expiry-tracker-v2";
 const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json",
   "/service-worker.js",
+  "/style.css",
   "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js"
 ];
 
 self.addEventListener("install", event => {
